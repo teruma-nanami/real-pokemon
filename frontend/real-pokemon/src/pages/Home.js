da02d9232b77417e2c018797886710f3c8d8a537
teruma-nanami <kinos.travel.fun@gmail.com>
2024-12-28 16:08:39 +0900

    react install

---
+++ frontend/real-pokemon/src/pages/Home.js
@@ -0,0 +1,13 @@
+// src/pages/Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <p>This is a static page built with React.</p>
+    </div>
+  );
+};
+
+export default Home;
